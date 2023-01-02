bc8ed6f0bc0e183b00bfc0a5fc3e304c7e33e230
Mohtashim-1 <64360314+Mohtashim-1@users.noreply.github.com>
2023-01-03 01:25:14 +0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Nav from "./components/Nav/Nav";
+// import "./react-router-dom";
+import React from "react";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
+import Nav from "./components/Nav/Nav"
+import Carousel from "./components/carousel/Carousel";
+import Card from "./components/Card/Card";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Carousel />
+      <Card />
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className="card-main">
+      <h1>Coarse</h1>
+      <div className="card-main-first">
+        <div className="card">
+          <img src="./pic/second.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="./pic/second.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="./pic/second.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="./pic/second.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card">
+          <img src="./pic/second.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
